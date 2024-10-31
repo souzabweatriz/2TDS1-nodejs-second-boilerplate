7b792d70ccb41c58abae1ec50af45feb2f2edccc
Bea Souza <158209681+souzabweatriz@users.noreply.github.com>
2024-10-31 16:45:33 -0300

    feat: create user by id method

--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -1,10 +1,8 @@
 import { Router } from "express";
-import UsersRepository from "../models/users/UsersRepository.js";
+import UsersRepository from "../models/users/usersRepository.js";
 
 const usuariosRoutes = Router();
 const usersList = new UsersRepository
-
-let usuarios = [];
 
 usuariosRoutes.get("/", (req, res) => {
   const usuarios = usersList.getAllusers()
@@ -18,13 +16,35 @@ usuariosRoutes.get("/", (req, res) => {
 });
 
 usuariosRoutes.post("/", (req,res) => {
-  const {name, email, password} = req.body
+  const {name, email, password} = req.body;
 
-  const user = usersList.addUser(name, email, password)
+  const user = usersList.addUser(name, email, password);
 
   return res.status(201).json({
-    message: "Usuário cadastrado com sucesso", user,
+    message: "Usuário cadastrado com sucesso", 
+    user,
   })
 })
 
+usuariosRoutes.get("/:id", (req,res) => {
+  const { id } = req.params;
+
+  const user = usersList.getUserById(id);
+
+  if(!user){
+    return res.status(404).json({
+      message: `Usuário com id ${id} não encontrado!`,
+    });
+  }
+  return res.status(200).json({
+    message:`Usuário com Id ${id} encontrado!`, 
+    user,
+  })
+})
+usuariosRoutes.put("/:id", (req,res) => {
+  
+})
+usuariosRoutes.delete("/:id", (req,res) => {
+  
+})
 export default usuariosRoutes;
