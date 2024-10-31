7ce53e2bbd697f55206d4976b4c7d084b46cee60
Bea Souza <158209681+souzabweatriz@users.noreply.github.com>
2024-10-31 17:15:29 -0300

    feat: create update user method

--- src/models/users/UsersRepository.js
+++ src/models/users/UsersRepository.js
@@ -25,6 +25,17 @@ class UsersRepository{
      }
      return user;
     }
+    updateUser(id, name, email, password){
+        const user = this.getUserById(id);
+        if(!user){
+            return null
+        }
+        user.name = name;
+        user.email = email;
+        user.password = password;
+
+        return user;
+    }
 }
 
 export default UsersRepository;
